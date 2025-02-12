38aec60153a4c2d48a4e1fa65e8e02ce981d75d5
Wesley C <wrcar213@learner.dvusd.org>
2025-02-12 09:10:08 -0700

    started car quartet game

---
+++ Quartet/CarCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quartet
+{
+    internal class CarCard
+    {
+       public int maxspeed {  get; set; }
+        public double zerotosixty { get; set; }
+        public int HP { get; set; }
+        public int CC { get; set; }
+        public int Cylinders { get; set; }
+        public int RPM { get; set; }
+    }
+}
